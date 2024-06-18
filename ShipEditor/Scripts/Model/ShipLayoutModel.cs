1ab123d9f07a44496d690722b68bc3c2e2619d8e
PavelZinchenko <zipas.games@gmail.com>
2024-06-18 14:21:30 +0400

    fix: ensure the ship editor functions correctly when the number of ship weapon barrels does not match the number of barrels in the layout

--- ShipEditor/Scripts/Model/ShipLayoutModel.cs
+++ ShipEditor/Scripts/Model/ShipLayoutModel.cs
@@ -45,15 +45,16 @@ namespace ShipEditor.Model
 
 		public Barrel Barrel(int x, int y)
 		{
-			var id = _barrelMap[x, y];
-			return id >= 0 ? _barrels[id] : null;
-		}
+            var id = _barrelMap[x, y];
+            return id >= 0 ? _barrels[id] : null;
+        }
 
-		public ShipLayoutModel(ShipElementType elementType, IShipLayout layout, ImmutableCollection<Barrel> barrels, IComponentTracker tracker)
+        public ShipLayoutModel(ShipElementType elementType, IShipLayout layout, ImmutableCollection<Barrel> barrels, IComponentTracker tracker)
 		{
 			_layout = layout;
 			_barrels = barrels;
-			_barrelMap.Build(layout);
+			_barrelMap.Build(layout, barrels.Count);
+
 			_tracker = tracker;
 			_elementType = elementType;
 		}
@@ -165,8 +166,8 @@ namespace ShipEditor.Model
 				if (requiredSlot == WeaponSlotType.Default) return true;
 
 				var barrelId = _barrelMap[x, y];
-				var barrel = _barrels[barrelId];
-
+                if (barrelId < 0) return false;
+                var barrel = _barrels[barrelId];
 				return string.IsNullOrEmpty(barrel.WeaponClass) || barrel.WeaponClass.Contains((char)requiredSlot);
 			}
 
@@ -207,10 +208,10 @@ namespace ShipEditor.Model
 
 		private int GetBarrelId(int x, int y, Layout layout)
 		{
-			for (int i = 0; i < layout.Size; ++i)
-				for (int j = 0; j < layout.Size; ++j)
-					if ((CellType)layout[j, i] != CellType.Empty)
-						return _barrelMap[x + j, y + i];
+            for (int i = 0; i < layout.Size; ++i)
+                for (int j = 0; j < layout.Size; ++j)
+                    if ((CellType)layout[j, i] != CellType.Empty)
+                        return _barrelMap[x + j, y + i];
 
 			return -1;
 		}
